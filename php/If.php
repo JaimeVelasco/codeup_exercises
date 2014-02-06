bdbd7d287882a344f3eb577693aadf088a1698d1
JaimeVelasco <jaime@photojv.com>
2014-02-06 11:50:19 -0600

    First exercise

---
+++ php/If.php
@@ -0,0 +1,39 @@
+<?php 
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+if ($a < $b) {
+	echo "$a is smaller than $b\n";
+}
+
+if ($b > $a) {
+	echo "$b is greater than $a\n";
+}
+
+if ($b >= $c) {
+	echo "$b is greater than or equal to $c\n";
+}
+
+if ($b <= $c) {
+	echo "$b is less than or equal to $c\n";
+}
+
+if ($b == $c) {
+	echo "$b is equal to $c\n";
+}
+
+
+if ($b != $c){
+	echo "$b is not equal to $c\n";
+}
+
+
+if ($b !== $c){
+	echo "$b is not identical to $c\n";
+}
+
+
+
+
