387b4968a5aebdf739fd4a9b24a3ba7dbe31597b
JaimeVelasco <jaime@photojv.com>
2014-02-06 14:07:56 -0600

    2nd exercise, if logical

---
+++ php/if_logical.php
@@ -0,0 +1,35 @@
+<?php
+
+$x = 0;
+$y = 5;
+$z =10;
+
+if ($x < $y && $y < $z) {
+	echo "$x < $y < $z\n";
+}
+
+if ($x > 0 || $x < 10) {
+	echo "$x is greater than 0 OR less than 10\n";
+}
+
+if ($y > 0 || $y < 10) {
+	echo "$y is greater than 0 OR less than 10\n";
+}
+
+if ($z > 0 || $z < 10) {
+	echo "$z is greater than 0 OR less than 10\n";
+}
+
+if ($x > 0 && $x < 10) {
+	echo "$x is greater than 0 AND less than 10\n";
+}
+
+if ($y > 0 && $y < 10) {
+	echo "$y is greater than 0 AND less than 10\n";
+}
+
+if ($z > 0 && $z < 10) {
+	echo "$z is greater than 0 AND less than 10\n";
+}
+
+?>
