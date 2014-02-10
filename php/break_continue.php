3de19e2bbe24f73de9dd3c505a43f1b8b63f8000
JaimeVelasco <jaime@photojv.com>
2014-02-10 14:50:46 -0600

    Print numbers from 1 to 100 but stops after 10 using brake.

--- php/break_continue.php
+++ php/break_continue.php
@@ -4,9 +4,16 @@
 //Create a for loop that shows all even numbers between 1 and 100 using continue.
 
 for ($i = 1 ; $i <= 100; $i++){
-	if($i % 2 == 0){
-	echo "$i\n";
-	continue;
-	}
+	echo $i . "\n";
 	
-}
+	// if($i % 2 == 0){
+	// echo "$i\n";
+	// continue;
+
+
+//Create another for loop that counts from 1 to 100, but stops after 10 using break.
+	if ($i == 10){
+		break;
+	}
+
+}	
