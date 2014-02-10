f9a67131e4561b50d105a3ca611918d8a4268982
JaimeVelasco <jaime@photojv.com>
2014-02-10 15:53:14 -0600

    Add a default case for the weekend

--- php/switch.php
+++ php/switch.php
@@ -7,6 +7,39 @@ date_default_timezone_set('America/Chicago');
 // 1 (for Monday) through 7 (for Sunday)
 $day_of_week = rand(1,7);
 
+// switch($day_of_week) {
+//     case 1:
+//         // Output Monday
+//     	echo "Monday\n";
+//     	break;
+//     case 2: 
+//         // Output Tuesday
+//     	echo "Tuesday\n";
+//     	break;
+//     case 3:
+//         // Output Wednesday
+//     	echo "Wednesday\n";
+//     	break;
+//     case 4: 
+//         // Output Thursday
+//     	echo "Thursday\n";
+//     	break;
+//     case 5: 
+//         // Output Friday
+//     	echo "Friday\n";
+//     	break;
+//     case 6:
+//         // Output Saturday
+//     	echo "Saturday\n";
+//     	break;
+//     case 7: 
+//         // Output Sunday
+//     	echo "Sunday\n";
+//     	break;
+
+
+// }
+
 switch($day_of_week) {
     case 1:
         // Output Monday
@@ -28,14 +61,6 @@ switch($day_of_week) {
         // Output Friday
     	echo "Friday\n";
     	break;
-    case 6:
-        // Output Saturday
-    	echo "Saturday\n";
-    	break;
-    case 7: 
-        // Output Sunday
-    	echo "Sunday\n";
-    	break;
-
-
-}
+    default: 
+    	echo "It's the weekend!!!\n";
+}    		
