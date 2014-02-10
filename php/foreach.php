89ff8ae1802f53e4b102c00fca2dff91a31bec0b
JaimeVelasco <jaime@photojv.com>
2014-02-10 11:12:32 -0600

    Output data types.

---
+++ php/foreach.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+//Construct a loop that iterates through each value and outputs its type as either integer, 
+//float, boolean, array, null, or string.
+
+
+foreach ($things as $data_type) {
+
+	if (is_int($data_type)){
+		echo "{$data_type} is an integer\n";
+	}
+	elseif(is_float($data_type)) {
+		echo "{$data_type} is a float\n";	
+	}
+	elseif (is_bool($data_type)) {
+		echo "{$data_type} is a boolean\n";	
+	}
+	elseif (is_array($data_type)) {
+		echo "{$data_type} is an array\n";
+	}
+	elseif (is_null($data_type)) {
+		echo "{$data_type} is null\n";
+	}
+	elseif (is_string($data_type)) {
+		echo "{$data_type} is a string\n";
+	}
+
+}
