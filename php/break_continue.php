66b539fa35d93c3a80334e1c549e84aa9bf9d134
JaimeVelasco <jaime@photojv.com>
2014-02-10 14:55:51 -0600

    New changes after review

--- php/break_continue.php
+++ php/break_continue.php
@@ -4,7 +4,7 @@
 //Create a for loop that shows all even numbers between 1 and 100 using continue.
 
 for ($i = 1 ; $i <= 100; $i++){
-	echo $i . "\n";
+	
 	
 	// if($i % 2 == 0){
 	// echo "$i\n";
@@ -12,8 +12,8 @@ for ($i = 1 ; $i <= 100; $i++){
 
 
 //Create another for loop that counts from 1 to 100, but stops after 10 using break.
-	if ($i == 10){
+	if ($i > 10){
 		break;
 	}
-
+echo $i . "\n";
 }	
