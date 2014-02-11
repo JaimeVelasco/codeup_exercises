6c63236b153364d8674c22d66479420f1fea2443
JaimeVelasco <jaime@photojv.com>
2014-02-11 11:09:13 -0600

    Add modulus function

--- php/Arithmetic.php
+++ php/Arithmetic.php
@@ -16,7 +16,14 @@ function divide($a, $b) {
    echo ($a / $b) . "\n";
 }
 
+function modulus($a, $b) {
+	echo ($a % $b) . "\n";
+}
+
+
+
 add(21, 5);
 subtract(20, 53);
 multiply(4, 6);
 divide(200, 3);
+modulus(3, 5);
