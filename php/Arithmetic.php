0cb24112c468953fd30ff4a5a91d21cc2b651db4
JaimeVelasco <jaime@photojv.com>
2014-02-11 11:07:24 -0600

    Step 2

---
+++ php/Arithmetic.php
@@ -0,0 +1,22 @@
+<?php
+
+function add($a, $b) {
+    echo ($a + $b) . "\n";
+}
+
+function subtract($a, $b) {
+    echo ($a - $b) . "\n";
+}
+
+function multiply($a, $b) {
+    echo ($a * $b) . "\n";
+}
+
+function divide($a, $b) {
+   echo ($a / $b) . "\n";
+}
+
+add(21, 5);
+subtract(20, 53);
+multiply(4, 6);
+divide(200, 3);
