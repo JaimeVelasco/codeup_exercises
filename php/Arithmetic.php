76cd86907f0624221f913113ce2baef9c5625d96
JaimeVelasco <jaime@photojv.com>
2014-02-11 13:25:04 -0600

    Function arguments step 1

--- php/Arithmetic.php
+++ php/Arithmetic.php
@@ -1,29 +1,50 @@
 <?php
 
+
+//Validate all the arguments and display an error if not numeric
+function validate($a, $b) {
+    if (!is_numeric($a) || !is_numeric($b)) {
+    	
+	    echo "ERROR: both values must be numbers" . "\n";
+	    exit(0);
+	}
+}
+
 function add($a, $b) {
+    validate ($a, $b);
     echo ($a + $b) . "\n";
 }
 
 function subtract($a, $b) {
+    validate ($a, $b);
     echo ($a - $b) . "\n";
 }
 
 function multiply($a, $b) {
+    validate ($a, $b);
     echo ($a * $b) . "\n";
 }
 
+
+//echo error if user tries to divide by 0.
+
 function divide($a, $b) {
-   echo ($a / $b) . "\n";
+   validate ($a, $b);
+  //  if ($a === 0 || $b === 0) {
+  //     	echo "Cannot divide by 0 dummy \n";
+  //  }else{
+  //     	echo ($a / $b) . "\n";
+		// }
 }
 
 function modulus($a, $b) {
+	validate ($a, $b);
 	echo ($a % $b) . "\n";
 }
 
+    	add(3, 5);
+		subtract(20, 12);
+		multiply(4, 8);
+		divide(200, 0);
+		modulus(3, 5);
 
-
-add(21, 5);
-subtract(20, 53);
-multiply(4, 6);
-divide(200, 3);
-modulus(3, 5);
