ff546e5d9862dcebbe8daa6849bd40ac79ad6165
JaimeVelasco <jaime@photojv.com>
2014-02-12 14:11:20 -0600

    Updated TODO list step 2.

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -12,7 +12,8 @@ $items = array();
 function list_items($list){
    $newvar =  ""; 
     foreach ($list as $key => $value) {
-     $newvar .= $value . PHO_EOL;
+        $key++;   
+        $newvar .= "$key-" . "$value\t" . " Item-" . $key . PHP_EOL;
     }
     return $newvar;
 }
@@ -20,49 +21,44 @@ function list_items($list){
     
 // Get STDIN, strip whitespace and newlines, 
 // and convert to uppercase if $upper is true
+
 function get_input($upper = FALSE) {
+
     // Return filtered STDIN input
     $input = trim(fgets(STDIN));
     return $upper ? strtoupper($input) : $input;
 }
 
 
-
-
-
-
         
 // The loop!
-do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {        
-        $key++;
-        // Display each item and a newline
-        echo "[{$key}] {$item}\n";
-    }
+do {         
+     // Echo the list produced by the function    
+    echo list_items($items);
 
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
-
-    // Get the input from user
-    // Use trim() to remove whitespace and newlines
-    $input = strtoupper(trim(fgets(STDIN)));
+    $input = get_input(TRUE);
 
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
+         $items[] = trim(fgets(STDIN));
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
     } elseif ($input == 'R') {
+       
         // Remove which item?
         echo 'Enter item number to remove: ';
+        
         // Get array key
-        $key = trim(fgets(STDIN)-1);
+        $key = get_input(TRUE)-1;
+       
         // Remove from array
         unset($items[$key]);
         $items = array_values($items);
     }
+
 // Exit when input is (Q)uit
 } while ($input != 'Q');
 
